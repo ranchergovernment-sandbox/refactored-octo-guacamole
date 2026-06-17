21a976a1d316f6de113a29973be7663007c7a5e4
Charles Randall <charlesrandall@ranchergovernment.com>
2026-06-17 10:00:28 -0700

    initial sync

---
+++ hauler.md
@@ -0,0 +1,6 @@
+# Install Hauler and sync content
+
+- Pull and install the binary as root
+    ```
+    curl -sfL https://get.hauler.dev | bash
+    ```
---
+++ login.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+. ./env.sh
+
+#hauler login registry://${offline_registry} -u admin -p <password>
+#hauler login registry.ranchercarbide.dev -u <username> -p <password>
