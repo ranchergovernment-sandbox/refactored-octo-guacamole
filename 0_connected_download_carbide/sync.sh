6ee3f31705c8bf1734e8423007b8e5d5a6577784
Charles Randall <charlesrandall@ranchergovernment.com>
2026-07-22 10:35:28 -0700

    clean up carbide

--- 0_connected_download_carbide/sync.sh
+++ 0_connected_download_carbide/sync.sh
@@ -9,26 +9,22 @@ sync_product rancher ${RANCHER_VERSION}
 
 sync_product airgapped-docs ${AIRGAP_DOCS_VERSION}
 
-sync_product cert-manager ${CERT_MANAGER_VERSION}
-
 sync_product harvester ${HARVESTER_VERSION}
 
-sync_product longhorn ${LONGHORN_VERSION}
+#sync_product longhorn ${LONGHORN_VERSION}
 
-sync_product neuvector ${NEUVECTOR_VERSION}
+#sync_product neuvector ${NEUVECTOR_VERSION}
 
-sync_product kubewarden kubewarden-controller-${KUBEWARDEN_CONTROLLER_VERSION} https://charts.kubewarden.io#!/bin/bash
+#sync_product kubewarden kubewarden-controller-${KUBEWARDEN_CONTROLLER_VERSION} https://charts.kubewarden.io#!/bin/bash
 
 sync_charts rancher rancher ${RANCHER_CHART_VERSION} oci://registry.ranchercarbide.dev/carbide-charts
 
 sync_charts airgapped-docs airgapped-docs ${AIRGAP_DOCS_CHART_VERSION} oci://registry.ranchercarbide.dev/carbide-charts
 
-sync_charts cert-manager cert-manager ${CERT_MANAGER_CHART_VERSION} oci://quay.io/jetstack/charts
+#sync_charts longhorn longhorn ${LONGHORN_CHART_VERSION} https://charts.longhorn.io
 
-sync_charts longhorn longhorn ${LONGHORN_CHART_VERSION} https://charts.longhorn.io
+#sync_charts neuvector core ${NEUVECTOR_CORE_CHART_VERSION} https://neuvector.github.io/neuvector-helm
+#sync_charts neuvector crd ${NEUVECTOR_CRD_CHART_VERSION} https://neuvector.github.io/neuvector-helm
+#sync_charts neuvector monitor ${NEUVECTOR_MONITOR_CHART_VERSION} https://neuvector.github.io/neuvector-helm
 
-sync_charts neuvector core ${NEUVECTOR_CORE_CHART_VERSION} https://neuvector.github.io/neuvector-helm
-sync_charts neuvector crd ${NEUVECTOR_CRD_CHART_VERSION} https://neuvector.github.io/neuvector-helm
-sync_charts neuvector monitor ${NEUVECTOR_MONITOR_CHART_VERSION} https://neuvector.github.io/neuvector-helm
-
-sync_charts kubewarden kubewarden-controller ${KUBEWARDEN_CONTROLLER_CHART_VERSION} https://charts.kubewarden.io
+#sync_charts kubewarden kubewarden-controller ${KUBEWARDEN_CONTROLLER_CHART_VERSION} https://charts.kubewarden.io
